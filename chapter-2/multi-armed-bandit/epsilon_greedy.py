a34eeee4b740895a36c34e233ca0d92b577a4640
Renz Iver Baliber <renz.baliber@senti.com.ph>
2019-12-02 22:27:14 +0800

    feat: add epsilon greedy k-armed bandit

---
+++ chapter-2/multi-armed-bandit/epsilon_greedy.py
@@ -0,0 +1,65 @@
+import matplotlib.pyplot as plt
+import numpy as np 
+import random
+
+class GreedyAgent:
+    def __init__(self, k, epsilon):
+        self.k = k
+        self.epsilon = epsilon
+        self.actions = [index for index in range(k)]
+        self.true_values = [np.random.normal(0,1) for x in range(k)]
+        self.q_values = np.zeros(self.k)
+        self.nth_action = np.zeros(k).tolist()
+        self.rewards = np.zeros(k).tolist()
+        self.ave_rewards = []
+
+    def run(self, steps):
+        for step in range(1,steps+1):
+            selected_act = select_action(self.epsilon, self.q_values, self.actions)
+            reward(self.rewards, self.true_values[selected_act], selected_act)
+            increase_nth(self.nth_action, selected_act)
+            update_qval(self.q_values,self.nth_action, self.rewards, selected_act)
+            _qval = [x/y if y else 0 for x,y in zip(self.q_values,self.nth_action)]
+            self.ave_rewards.append(np.mean(_qval))
+            print(self.nth_action)
+        
+        
+        ## Problem averaging - per step get qvalues / nth action
+            
+        x = [x for x in range(steps)]
+        y = self.ave_rewards
+        plt.figure(figsize=(10, 20))
+        plt.plot(x, y)
+        plt.xlabel('steps')
+        plt.ylabel('average reward')
+        plt.legend()
+        plt.title("Epsilon Greedy Multi-Armed Bandit | Number of Bandits: {} Epsilon: {}".format(self.k, self.epsilon))
+        plt.show()
+        return True
+            
+        
+
+def select_action(epsilon, q_values, actions):
+    val = np.random.randn()
+    if val < epsilon:
+        action = random.choice(actions)
+    else:
+        action = np.argmax(q_values)
+    return action
+
+def reward(rewards,true_values, action):
+    rewards[action] = np.random.normal(true_values,1) 
+    return rewards
+
+def increase_nth(nth_action, action):
+    nth_action[action] += 1
+    return nth_action
+
+def update_qval(q_values,nth_action, reward, action):
+    q_values[action] = q_values[action] + ((reward[action]-q_values[action])/nth_action[action])
+    return q_values
+
+    
+
+        
+    
---
+++ chapter-2/multi-armed-bandit/main.py
@@ -0,0 +1,4 @@
+from epsilon_greedy import GreedyAgent
+
+agent = GreedyAgent(10, 0.1)
+agent.run(steps=2000)
